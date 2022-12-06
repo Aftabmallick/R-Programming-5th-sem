39c428bd7676997c17ea62bc4e544346c75d19e5
Aftab Mallick <78075420+Aftabmallick@users.noreply.github.com>
2022-12-06 23:21:21 +0530

    Create Assignemnt17.R

---
+++ Assignemnt17.R
@@ -0,0 +1,16 @@
+f<-function(x,m,sd){
+  return(exp((-(x-m)^2)/(2*sd^2))/(sd*sqrt(2*3.14)))
+}
+
+up<-4200
+lb<-2500
+m<-4300
+sd<-750
+n<-1000
+h<-(up-lb)/n
+s=0.0
+for(i in 0:n){
+  s<-s+f(lb+i*h,m,sd)+f(lb+(i+1)*h,m,sd)
+}
+s<-s*h/2.0
+print(s)
